6675136e59fb52731995a34d51c2307c5d1d0d67
Rafsanjani605 <janirafsan605@gmail.com>
2023-02-09 18:38:03 +0600

    server

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 8000;
+
+
+app.use(cors());
+
+
+
+const categories = require('./data/Categories.json')
+
+
+app.get('/' , (req , res) =>{
+    res.send('news api running')
+})
+
+app.get('/news-category' , (req , res) =>{
+    res.send(categories)
+})
+
+app.listen(port , () => {
+    console.log('dragon news:' , port)
+})
